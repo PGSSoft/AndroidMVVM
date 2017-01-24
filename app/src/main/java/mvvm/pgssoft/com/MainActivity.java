95abe964084a5218bd6c96a1f6630b300985c031
Bartek Stokrocki <bartek.stokrocki@gmail.com>
2017-01-24 21:27:18 +0100

    Initial commit

---
+++ app/src/main/java/mvvm/pgssoft/com/MainActivity.java
@@ -0,0 +1,15 @@
+package mvvm.pgssoft.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mvvm.pgssoft.com.mvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
